d5a871448da61a58e3c7fde0ec44915be906d3c6
Adam Green <adamgreen@users.noreply.github.com>
2018-07-14 15:34:33 -0700

    Updated Radar sample
    Now just runs in an infinite loop displaying text to the user whenever
    the radar detects a change in the distance to an obstacle.

--- examples/Radar.c
+++ examples/Radar.c
@@ -16,6 +16,7 @@
     mipGetLatestRadarNotification()
 */
 #include <stdio.h>
+#include <string.h>
 #include "mip.h"
 #include "osxble.h"
 
@@ -30,21 +31,48 @@ int main(int argc, char *argv[])
 
 void robotMain(void)
 {
-    int                  result = -1;
-    MiPRadarNotification radar = {0, MIP_RADAR_NONE};
-    MiP*                 pMiP = mipInit(NULL);
+    int     result = -1;
+    MiP*    pMiP = mipInit(NULL);
 
-    printf("\tRadar.c - Use mipGetLatestRadarNotification() function.\n"
-           "\tProgram should end once you place your handle <10cm from MiP's face.\n");
+    printf("\tRadar.c - Display current radar readings to user.\n");
 
     // Connect to first MiP robot discovered.
-    result = mipConnectToRobot(pMiP, NULL);
+    mipConnectToRobot(pMiP, NULL);
 
-    result = mipSetGestureRadarMode(pMiP, MIP_RADAR);
-    do
+    printf("\tWaiting for robot to be standing upright.\n");
+    MiPStatus status = {0, 0.0f, 0};
+    while (MIP_ERROR_NONE != mipGetLatestStatusNotification(pMiP, &status) && status.position != MIP_POSITION_UPRIGHT)
     {
-        result = mipGetLatestRadarNotification(pMiP, &radar);
-    } while (radar.radar != MIP_RADAR_0CM_10CM);
+    }
+
+    mipSetGestureRadarMode(pMiP, MIP_RADAR);
+
+    static MiPRadarNotification lastRadar = { 0, -1 };
+    while (1)
+    {
+        MiPRadarNotification currentRadar;
+        result = mipGetLatestRadarNotification(pMiP, &currentRadar);
+        if (result != MIP_ERROR_NONE || 0 == memcmp(&lastRadar, &currentRadar, sizeof(lastRadar)))
+        {
+            continue;
+        }
+
+        printf("Radar = ");
+        switch (currentRadar.radar) {
+          case MIP_RADAR_NONE:
+            printf("None\n");
+            break;
+          case MIP_RADAR_10CM_30CM:
+            printf("10cm - 30cm\n");
+            break;
+          case MIP_RADAR_0CM_10CM:
+            printf("0cm - 10cm\n");
+            break;
+          default:
+            break;
+        }
+        lastRadar = currentRadar;
+    }
 
     mipUninit(pMiP);
 }
